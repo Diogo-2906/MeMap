404c0f115b8f55a2dcc5ca6741da47424767bd2c
Diogo-2906 <diogo.ozeliero@aluno.senai.br>
2025-10-21 10:56:35 -0300

    Adicionar arquivos de projeto.

---
+++ MeMap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MeMap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
